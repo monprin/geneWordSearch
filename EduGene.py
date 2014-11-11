a00fe4b3b5cf8c5a81e93acf3483e06edf2ec0ca
Joe Jeffers <jeffe174@umn.edu>
2014-11-10 19:32:55 -0600

    Added the beginings of the P value function to do word weights differently, currently it is not used due to the use of factorials that are effectively impossible to compute. Looking for alternative methods.

--- EduGene.py
+++ EduGene.py
@@ -20,6 +20,49 @@ def geneDBMaker():
 		db.append(row)
 		
 	return db
+	
+def wordCountDBMaker():
+	x = open('totalWordCount.txt')
+	db = []
+	
+	for line in x.readlines():
+		row = line.split('\t')
+		row[1] = row[1][:len(row[1])-1]
+		row[0] = int(row[0])
+		db.append(row)
+		
+	x.close()	
+	return db
+	
+def findPVal(word, DBRow, wordCountsDB):
+	# Overall population size; computed using numOfWords in WordStats.py
+	N = 1398197
+	# Finding the count of word in question
+	K = 0
+	for row in wordCountsDB:
+		if(word == row[1]):
+			K = row[0]
+			break
+	if(K==0):
+		raise ValueError('This is not a known word, please try again')
+	# Setting the size of the row it's found in
+	n = len(DBRow)
+	# Matches in the relevant row
+	count = 0
+	for x in DBRow:
+		if(word == x):
+			count += 1
+	k = count
+	
+	# Using the Hypergeometric function, we find a weight value
+	ans = (biCo(K,k)*biCo((N-K),(n-k)))/biCo(N,n)
+	return ans
+	
+	
+def biCo(x,y):
+	# Find the binomial coefficient of the given values
+	import math
+	return (math.factorial(x)/(math.factorial(y)*math.factorial(x-y)))
 
 def geneWordSearch(gene,db):
 	# Input: Takes in a gene identifier and the built database from the above function.
@@ -48,7 +91,7 @@ def geneWordSearch(gene,db):
 			listing.remove(entry)	
 	# Splitting the various strings into individual words per list item
 	for entry in listing:
-		words += re.split(' |-|_|,|\.',entry)	
+		words += re.split(' |_|,|\.',entry)	
 	wordList = list(filter(None,words))
 
 	# Building the infrastructure for counting the words
@@ -57,11 +100,8 @@ def geneWordSearch(gene,db):
 	pval = []
 	wordFreq = []
 	
-	# Adding the web link counts to the list
-	freq.append(links)
-	word.append('Web Links')
-	
 	# Counting the words while emptying the word list
+	row = wordList
 	while not(wordList == []):
 		item = wordList.pop()
 		if(item in word):
@@ -70,6 +110,20 @@ def geneWordSearch(gene,db):
 		else:
 			word.append(item)
 			freq.append(1)
+	
+	# Finding the respective P values
+	#i = 0
+	#wordCounts = wordCountDBMaker()
+	#while i < len(word):
+	#	val = findPVal(word[i], row, wordCounts)
+	#	pval.append(val)
+	#	i += 1
+	#del wordCounts
+	
+	# Adding the web link counts to the list
+	freq.append(links)
+	word.append('Web Links')
+	#pval.append(0)
 	
 	# Putting the frequency list together with the word list
 	i = 0
