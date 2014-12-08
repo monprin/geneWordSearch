51f1314b98355380f642b29a8e1aaa6be33d4ce0
monprin <jeffe174@umn.edu>
2014-12-08 16:11:57 -0600

    Moved GeneFreq class to the Classes file for cleaner file structure.

--- DBBuilder.py
+++ DBBuilder.py
@@ -1,35 +1,9 @@
 # Function to build the gene annotation database and pickle it
 
-class GeneNote:
-	# Class for holding a gene and all of the words that anotate it.
-	def __init__(self, gene):
-		self.gene = gene
-		self.words = []
-		self.links = []
-		
-	def addWord(self,word):
-		self.words.append(word)
-		
-	def addLink(self,link):
-		self.links.append(link)
-		
-	def toString(self):
-		ans = self.gene + '\t'
-		for word in self.words[:-1]:
-			ans += word + '\t'
-		if(self.links == [] and not(self.words == [])):
-			ans += self.words[-1]
-			return ans + '\n'
-		for link in self.links[:-1]:
-			ans += link + '\t'
-		if not(self.links == []):
-			ans += self.links[-1]
-		return ans + '\n'
-			
-		
 def geneWordBuilder(infile='databases/geneMatrix.txt',outfile='databases/geneNotes.txt'):
 	import re
 	import pickle
+	from Classes import GeneNote
 	
 	matrix = open(infile)
 	db = []
