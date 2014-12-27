3d6bbd8133fb670265e63c6b557f785a10e4389a
monprin <jeffe174@umn.edu>
2014-12-27 16:08:56 -0600

    Added support for running through all files in a given directory.

--- EduGene.py
+++ EduGene.py
@@ -120,6 +120,7 @@ if __name__ == '__main__':
 	parser.add_argument('-p',action='store',type=float,default=0.05,help='This option takes one argument and sets the probability cutoff, default is 0.2.')
 	parser.add_argument('-m',action='store_true',default=False,help='This will give a tsv output for machine readable purposes. Default is human readable.')
 	parser.add_argument('-f',action='store_true',default=False,help='This indicates that the input strings will be files with genes in them')
+	parser.add_argument('-d',action='store_true',default=False,help='Indicates that the input is a directory and will process all files in the directory')
 	parser.add_argument('things',action='store',nargs='*')
 	args = parser.parse_args()
 	if(args.f):
@@ -128,8 +129,29 @@ if __name__ == '__main__':
 			geneList = open(name)
 			for row in geneList.readlines():
 				genes += row.split()
-
+			geneWordSearch(genes,webLinks=args.w,minChance=args.p,machineRead=args.m)
+	
+	
+	elif(args.d):
+		import glob
+		
+		for folder in args.things:
+			if(not(folder[-1] == '/')):
+				folder += '/'
+			fileList = glob.glob(folder + '*.txt')
+			for fileName in fileList:
+				genes = []
+				geneList = open(fileName)
+				for row in geneList.readlines():
+					genes += row.split()
+				if((len(genes) >= 10) and (len(genes) <= 300)):
+					print('\n')
+					print('Results for ' + fileName + ':')
+					print('\n')
+					geneWordSearch(genes,webLinks=args.w,minChance=args.p,machineRead=args.m)
+				geneList.close()
+	
 	else:
 		genes = args.things
+		geneWordSearch(genes,webLinks=args.w,minChance=args.p,machineRead=args.m)
 	
-	geneWordSearch(genes,webLinks=args.w,minChance=args.p,machineRead=args.m)
