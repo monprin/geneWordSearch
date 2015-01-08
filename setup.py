68aa525b1b00ae97ba6502eca8bc00b929b1cac4
monprin <jeffe174@umn.edu>
2015-01-07 19:02:23 -0600

    Update

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ from setuptools import setup, find_packages
 setup(name='GeneWordSearch',
 version='0.9',
 description='Annotation finder for Maize genes.',
-author='Joe Jeffers',author_email='jeffe174@umn.edu',
+author='Joe Jeffers',
+author_email='jeffe174@umn.edu',
 packages=['Classes','GeneWordSearch']
 package_data={'databases/totalWordCounts.p':['databases/totalWordCounts.p'],'databases/geneNotes.p':['databases/geneNotes.p']},
 long_description=open('README.md').read(),
