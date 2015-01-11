c7a41a8f7ca5a3f8fd54f276ceefad7701194cf5
monprin <jeffe174@umn.edu>
2015-01-11 13:59:56 -0600

    Added support for looking up gene networks. only goes one level of network connections deep at the moment to preserve performance until further tests and optimizations can happen. Also made output go to file by default. Added corresponding argument to function.

--- src/CLI.py
+++ src/CLI.py
@@ -2,17 +2,23 @@
 # Command Line interface for geneWordSearch:
 from GeneWordSearch import geneWordSearch
 import sys
+import pickle
 import argparse
 from Classes import WordFreq
 
 parser = argparse.ArgumentParser(description='Find the important words associated with supplied genes.')
-parser.add_argument('-w',action='store_true',default=False,help='This will output associated weblinks with standard gene output.')
+parser.add_argument('-d',action='store_true',default=False,help='Indicates that the input is a directory and will process all files in the directory.')
+parser.add_argument('-f',action='store_true',default=False,help='This indicates that the input strings will be files with genes in them.')
+parser.add_argument('-n',action='store_true',default=False,help='Indicates that the input is the starting point of a network, will first return list of genes in those networks, then the traditional output on that list of genes.')
+parser.add_argument('-o',action='store',type=str,default='out.txt',help='Location to write the file that contains the results, default is out.txt in current folder.')
 parser.add_argument('-p',action='store',type=float,default=0.05,help='This option takes one argument and sets the probability cutoff, default is 0.2.')
 parser.add_argument('-t',action='store_true',default=False,help='This will give a tsv output for machine readable purposes. Default is human readable output.')
-parser.add_argument('-f',action='store_true',default=False,help='This indicates that the input strings will be files with genes in them')
-parser.add_argument('-d',action='store_true',default=False,help='Indicates that the input is a directory and will process all files in the directory')
+parser.add_argument('-w',action='store_true',default=False,help='This will output associated weblinks with standard gene output.')
 parser.add_argument('things',action='store',nargs='*')
 args = parser.parse_args()
+
+out = open(args.o,'w')
+
 if(args.f):
 	genes = []
 	for name in args.things:
@@ -21,6 +27,20 @@ if(args.f):
 			genes += row.split()
 		results = geneWordSearch(genes,minChance=args.p)
 
+elif(args.n):
+	genes = []
+	nets = open('databases/networks.p','rb')
+	networks = pickle.load(nets)
+	
+	for gene in args.things:
+		genes += networks[gene]
+	
+	out.write('These are the genes related to the gene(s) you identified:'+'\n')	
+	for gene in genes:
+		out.write(gene + '\n')
+	
+	out.write('\n' + 'Results from this list:' + '\n' + '\n')
+	results = geneWordSearch(genes,minChance=args.p)
 
 elif(args.d):
 	import glob
@@ -36,9 +56,9 @@ elif(args.d):
 			for row in geneList.readlines():
 				genes += row.split()
 			if((len(genes) >= 10) and (len(genes) <= 300)):
-				print('\n')
-				print('Results for ' + fileName + ':')
-				print('\n')
+				out.write('\n' + '\n')
+				out.write('Results for ' + fileName + ':')
+				out.write('\n' + '\n')
 				results += geneWordSearch(genes,minChance=args.p)
 			geneList.close()
 
@@ -54,22 +74,24 @@ links = results[1]
 # machineRead Parameter prints the output as a tsv sheet to be used for a machine readable format
 if(args.t):
 	# Print the header for the table
-	print(WordFreq.robotHeaders())
+	out.write(WordFreq.robotHeaders()+ '\n')
 	
 	# Print the lines using class function
 	for item in words:
-		print(item.forRobot())
+		out.write(item.forRobot() + '\n')
 	
 	if(args.w):
 		for link in links:
-			print(link)
+			out.write(link + '\n')
 	
 else:
 	
 	for item in words:
-		print(item.forHuman())
+		out.write(item.forHuman() + '\n')
 		
 	if(args.w):
-		print('Web Links associated with these genes:'+'\n')
+		out.write('Web Links associated with these genes:'+'\n' + '\n')
 		for link in links:
-			print(link + '\n')
+			out.write(link + '\n' + '\n')
+
+print('Completed! Check ' + args.o + ' for your results.')
