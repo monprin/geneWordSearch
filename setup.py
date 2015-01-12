a59ebfafe6bbd0afc743bbf94b52c80266983b45
monprin <jeffe174@umn.edu>
2015-01-12 13:03:36 -0600

    Addedneeded code for distribution on systems without setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 # Basic Setup Script
 # Requires Python3
 
+import ez_setup
+ez_setup.use_setuptools()
+
 from setuptools import setup, find_packages
 
 setup(name='GeneWordSearch',
-version='0.9',
+version='1.0',
+license='GPLv2',
 description='Annotation finder for Maize genes.',
 author='Joe Jeffers',
 author_email='jeffe174@umn.edu',
