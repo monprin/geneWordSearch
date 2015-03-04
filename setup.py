9f68c37a06c3e61f83bb0d64d3c0423f327fc9e6
monprin <jeffe174@umn.edu>
2015-03-04 14:25:01 +0800

    Updated with new code.

--- setup.py
+++ setup.py
@@ -2,16 +2,12 @@
 # Requires Python3
 
 # Written by Joseph Jeffers
-# Updated Jan 12 2015
-
-# Installs setuptools if needed
-from GeneWordSearch.src import ez_setup
-ez_setup.use_setuptools()
+# Updated Feb 15 2015
 
 from setuptools import setup, find_packages
 
 setup(name='GeneWordSearch',
-version='1.0',
+version='1.0.3',
 license='GPLv2',
 description='Annotation finder for Maize genes.',
 author='Joe Jeffers',
